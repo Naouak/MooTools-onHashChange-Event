cde6609433553b77109a3877f27bf799b3145d10
Greggory Hernandez <greggory.hz@gmail.com>
2010-07-22 14:17:37 -0600

    initial source code drop and prep for the forge

---
+++ README.md
@@ -0,0 +1,11 @@
+MooTools onHashChange Event
+===========================
+
+This is a simply script to add the onhashchange event to browsers that don't already support it.
+
+How to use
+----------
+
+Simply include the MooHashChange.js script and then you can set the event handler like this:
+
+	window.addEvent('hashchange', function () { // do stuff });
---
+++ Source/MooHashChange.js
@@ -0,0 +1,33 @@
+/*
+---
+description: Added the 'hashchange' event
+
+license: MIT-style
+
+authors:
+- sdf1981cgn (https://mootools.lighthouseapp.com/users/71792)
+- Greggory Hernandez
+
+requires:
+- core/1.2.4:Event
+*/
+Element.Events.hashchange = {
+    onAdd: function(){
+        var hash = self.location.hash;
+
+        var hashchange = function(){
+            if (hash == self.location.hash) return;
+            else hash = self.location.hash;
+
+            var value = (hash.indexOf('#') == 0 ? hash.substr(1) : hash);
+            window.fireEvent('hashchange', value);
+            document.fireEvent('hashchange', value);
+        };
+
+        if ("onhashchange" in window){
+            window.onhashchange = hashchange;
+        } else {
+            hashchange.periodical(50);
+        }
+    }
+};
