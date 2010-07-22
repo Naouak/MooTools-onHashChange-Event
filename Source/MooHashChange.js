3d7da386be3f6c501666c11593385e254cb6b843
Greggory Hernandez <greggory.hz@gmail.com>
2010-07-22 14:31:11 -0600

    added yml headers

--- Source/MooHashChange.js
+++ Source/MooHashChange.js
@@ -4,9 +4,10 @@ description: Added the 'hashchange' event
 
 license: MIT-style
 
-authors: [sdf1981cgn (https://mootools.lighthouseapp.com/users/71792), Greggory Hernandez]
+authors: [sdf1981cgn, Greggory Hernandez]
 
-requires: core/1.2.4: Event
+requires: 
+  core/1.2.4: Event
 
 provides: Element.Events.hashchange
 */
